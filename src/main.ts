c0ba5c60e1fda94796d1e37dc6bbce56d7e30f13
VALERY VIARENICH <vviarenich@hubspot.com>
2021-09-01 10:30:58 +0300

    - added enable cors mode

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   (app as any).set('etag', false);
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
+    res.set('Access-Control-Allow-Credentials', true);
     next();
   });
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
